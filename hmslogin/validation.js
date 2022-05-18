6eaf65e8ad33ea22bea9e35b7f1132cce32de02c
syedajmal05 <101813203+syedajmal05@users.noreply.github.com>
2022-05-18 14:12:48 +0530

    Add files via upload

---
+++ hmslogin/validation.js
@@ -0,0 +1,73 @@
+// var username = document.forms['form']['username'];
+// var password = document.forms['form']['password'];
+
+// var username_empty=document.getElementById('username_empty');
+// var password_empty=document.getElementById('password_empty');
+
+// username.addEventListener
+
+// function validated(){
+//     if(username.value.length < 1){
+//         username.style.border="1px solid red";
+//         username_empty.style.display ="block";
+//         username.focus();
+//         return false();
+//     }
+// }
+
+
+
+// var exampleInputName1 =document.querySelector('#exampleInputName1');
+// exampleInputName1.addEventListener('keyup',function(){
+//     var x = document.querySelector('.x');
+//     var check = document.querySelector('.check');
+//     if ( exampleInputName1.value.length ,1){
+//         exampleInputName1.getElementsByClassName.border='1px solid red';
+//         x.style.display ='block';
+//         check.style.display ='none';
+//         return false;
+//     }else{
+//         exampleInputName1.getElementsByClassName.border='1px solid green';
+//         x.style.display ='none';
+//         check.style.display ='block';
+//     }
+
+// })
+
+
+
+function validate()
+{
+    var username  =document.getElementById('exampleInputName1');
+    var password  =document.getElementById('exampleInputPassword1');
+
+    if(username.value =="" || password.value=="" )
+    {
+        alert("no blank allowed");
+    }
+    else{
+        
+    }
+    
+}
+
+
+
+
+// var username = document.forms['form']['username'];
+// var password = document.forms['form']['password'];
+
+// var username_empty=document.getElementById('x');
+// var password_empty=document.getElementById('check');
+
+// username.addEventListener
+
+// function validated(){
+//     if(username.value.length ==""|| password.value.length==""){
+//         username.style.border="20px solid red";
+//         password.style.border="1px solid red";
+//         username_empty.style.display ="block";
+//         password_empty.style.display ="none";
+//         return false();
+//     }
+// }
